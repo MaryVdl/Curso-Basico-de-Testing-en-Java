410c302c7d0db5b3a3e7d2b0d740ad734a497b44
MaryVdl <marisolvidal323@gmail.com>
2022-07-06 17:25:26 -0500

    Junit para verificar password

--- src/main/java/comm/platzi/javatests/util/PasswordUtil.java
+++ src/main/java/comm/platzi/javatests/util/PasswordUtil.java
@@ -1,2 +1,20 @@
-package comm.platzi.javatests.util;public class PasswordUtil {
+package comm.platzi.javatests.util;
+
+public class PasswordUtil {
+
+    public enum SecurityLevel{
+        WEAK, MEDIUM, STRONG
+    }
+
+    public static SecurityLevel assessPassword(String password){
+        //matches para uso de expresiones regulares
+        if (password.length()<8 | password.matches("[a-zA-Z]+")) {
+            return SecurityLevel.WEAK;
+        }
+        if (password.matches("[a-zA-Z0-9]+")){
+            return SecurityLevel.MEDIUM;
+        }
+
+        return SecurityLevel.STRONG;
+    }
 }
--- src/test/java/comm/platzi/javatests/util/PasswordUtilTest.java
+++ src/test/java/comm/platzi/javatests/util/PasswordUtilTest.java
@@ -1,4 +1,31 @@
+package comm.platzi.javatests.util;
+
+import org.junit.Test;
+
+import static comm.platzi.javatests.util.PasswordUtil.SecurityLevel.*;
 import static org.junit.Assert.*;
+
 public class PasswordUtilTest {
-  
+
+    @Test
+    public void weak_when_has_less_than_8_letters(){
+        //Funcion de comprobacion de Junit (assertEquals)
+        assertEquals(WEAK,PasswordUtil.assessPassword("1234567"));
+    }
+
+    @Test
+    public void weak_when_has_only_letters(){
+        //Funcion de comprobacion de Junit (assertEquals)
+        assertEquals(WEAK,PasswordUtil.assessPassword("abcdefg"));
+    }
+    @Test
+    public void medium_then_has_letter_and_numbers(){
+        assertEquals(MEDIUM, PasswordUtil.assessPassword("1234abcde"));
+    }
+
+    @Test
+    public void strong_when_has_letters_numbers_and_simbols(){
+        assertEquals(STRONG, PasswordUtil.assessPassword("abc12!er!"));
+    }
+
 }
