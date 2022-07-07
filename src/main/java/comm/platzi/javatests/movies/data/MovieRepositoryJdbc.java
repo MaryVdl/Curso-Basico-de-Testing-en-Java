fc86111d7a21b2d5fdb537bb4cc493e5116a85da
MaryVdl <marisolvidal323@gmail.com>
2022-07-07 18:43:01 -0500

    Guardar y buscar películas individuales

--- src/main/java/comm/platzi/javatests/movies/data/MovieRepositoryJdbc.java
+++ src/main/java/comm/platzi/javatests/movies/data/MovieRepositoryJdbc.java
@@ -19,7 +19,8 @@ public class MovieRepositoryJdbc implements MovieRepository {
 
     @Override
     public Movie findById(long id) {
-        return null;
+        Object[] args = {id};
+        return jdbcTemplate.queryForObject("select * Fron movies where id = ?", args, movieMapper);
     }
 
     @Override
@@ -29,6 +30,8 @@ public class MovieRepositoryJdbc implements MovieRepository {
 
     @Override
     public void saveOrUpdate(Movie movie) {
+        jdbcTemplate.update("insert into movies(name, minutes, genre) values(?,?,?)",
+                movie.getName(), movie.getMinutes(), movie.getGenre().toString());
 
     }
 
